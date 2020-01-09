83222bb9a6d05961fc2f2f01b63d052a1752f381
Anand Raj <anand.raj@letsventure.com>
2020-01-09 09:41:31 +0530

    set view for login and logout user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    helper_method :currrent_user,:log_in?
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def log_in?
+        !!current_user
+    end
+
+    def require_user
+        if !log_in?
+            flash[:danger] = "you must log in to perform the action"
+            redirect_to root_path
+        end
+    end
+    
 end
