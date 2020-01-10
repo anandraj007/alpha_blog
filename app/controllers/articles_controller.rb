dbff96966401fb4ac70a3c4ea298c2f44a400fcf
Anand Raj <anand.raj@letsventure.com>
2020-01-10 11:00:17 +0530

    issue in production fixed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,7 +23,7 @@ class ArticlesController < ApplicationController
     @article.user = User.find(current_user)
     if @article.save
       flash[:success] ="Article is successfully created"
-      redirect_to articles_path
+      redirect_to article_path(@article.id)
     else
       render 'new'
     end
