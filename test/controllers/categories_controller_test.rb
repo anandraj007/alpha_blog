440a7a345bbbfd8a8aa87cf4be52764132b85f38
Anand Raj <anand.raj@letsventure.com>
2020-01-12 22:22:26 +0530

    added funcationality of only admin can create category

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,15 +4,16 @@ class CategoriesControllerTest <  ActionDispatch::IntegrationTest
 
     def setup
         @category = Category.create(name: "sports")
+        @user = User.create(username:"anand",email:"anand@gmail.com",password:"password",admin:true)
     end
     
-
     test "should have index of category" do
         get categories_path
         assert_response :success
     end
 
     test "should have new category" do
+        sign_in_as_admin(@user)
         get new_category_path
         assert_response :success
     end
@@ -20,5 +21,16 @@ class CategoriesControllerTest <  ActionDispatch::IntegrationTest
     test "should have show path of category" do
         get category_path(@category)
         assert_response :success
+    end
+
+    test"should redirect if user is not log in as admin" do
+        assert_no_difference "Category.count" do
+          post categories_path,params:{category:{name:"sport"}}
+        end
+        assert_redirected_to categories_path
+    end
+
+    def sign_in_as_admin(user)
+        post login_path,params:{session:{email:user.email,password:user.password}}
     end
 end
