10d6b45d99ea6867578740229a8fd2a4e9e28682
Anand Raj <anand.raj@letsventure.com>
2020-01-13 16:04:35 +0530

    add feature to edit category name by admin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -17,7 +17,21 @@ class CategoriesController < ApplicationController
             render 'new'
         end
     end
-    
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:success] = "category name was successfully updated"
+            redirect_to category_path(@category)
+        else
+            render 'edit'
+        end
+    end
+
     def show
         @category = Category.find(params[:id])
         @category_articles = @category.articles.paginate(page: params[:page],per_page:5)
@@ -30,7 +44,7 @@ class CategoriesController < ApplicationController
 
     def require_admin
         if !logged_in? || (logged_in? and !current_user.admin?)
-            flash[:danger] = 'only admin can create category'
+            flash[:danger] = 'only admin can create and update category'
             redirect_to categories_path
         end
     end
